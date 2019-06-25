d824fa74beecd86368a5729b677c0b7ad0830668
tavisca-ankumar <ankumar@tavisca.in>
2019-06-25 23:05:45 +0530

    committing changes after giving modularity and naming convention to the given solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/FixMultiplication.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/FixMultiplication.cs
@@ -23,66 +23,103 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            String saveUnknownInteger = "", valueInString = "";
-            int saveKnownInteger1 = 0, saveKnownInteger2 = 0, value = 0;
-            Boolean x = true;
+            var MissingOperand = "";  //storing Missing operand
+            var CalculatedMissingOperandInString = ""; //storing Missing operand in string format
+            var checkForConditions=-1;  
+            int FirstOperand = 0, SecondOperand = 0, CalculatedMissingOperand = 0;
             String[] splitOnMultiplicationSign = equation.Split('*');
+            //when 2?*22=484 first operand is missing
             if (splitOnMultiplicationSign[0].Contains("?"))
             {
-                saveUnknownInteger = splitOnMultiplicationSign[0];
-                saveKnownInteger1 = Int32.Parse(splitOnMultiplicationSign[1].Split('=')[0]);
-                saveKnownInteger2 = Int32.Parse(splitOnMultiplicationSign[1].Split('=')[1]);
-                value = (saveKnownInteger2 / saveKnownInteger1);
-                valueInString = value.ToString();
-                if (Math.Ceiling((decimal)((decimal)saveKnownInteger2 / (decimal)saveKnownInteger1)) != value)
-                {
-                    x = false;
-                }
+                MissingOperand = splitOnMultiplicationSign[0];
+                String[] splitOnEqualsSign = splitOnMultiplicationSign[1].Split('=');
+                FirstOperand = Int32.Parse(splitOnEqualsSign[0]);
+                SecondOperand = Int32.Parse(splitOnEqualsSign[1]);
+                CalculatedMissingOperand = (SecondOperand / FirstOperand);
+                CalculatedMissingOperandInString = CalculatedMissingOperand.ToString();
+                checkForConditions = VerifyIfMissingOperandIsCorrect(FirstOperand, CalculatedMissingOperand, SecondOperand, '/');
             }
             else if (splitOnMultiplicationSign[1].Contains("?"))
             {
-                saveKnownInteger1 = Int32.Parse(splitOnMultiplicationSign[0]);
+                FirstOperand = Int32.Parse(splitOnMultiplicationSign[0]);
                 String[] splitOnEqualsSign = splitOnMultiplicationSign[1].Split('=');
+                //when 22*2?=484 second operand is missing
                 if (splitOnEqualsSign[0].Contains("?"))
                 {
-                    saveUnknownInteger = splitOnEqualsSign[0];
-                    saveKnownInteger2 = Int32.Parse(splitOnEqualsSign[1]);
-                    value = (saveKnownInteger2 / saveKnownInteger1);
-                    valueInString = value.ToString();
-                    if (Math.Ceiling((decimal)((decimal)saveKnownInteger2 / (decimal)saveKnownInteger1)) != value)
-                    {
-                        x = false;
-                    }
+                    MissingOperand = splitOnEqualsSign[0];
+                    SecondOperand = Int32.Parse(splitOnEqualsSign[1]);
+                    CalculatedMissingOperand = (SecondOperand / FirstOperand);
+                    CalculatedMissingOperandInString = CalculatedMissingOperand.ToString();
+                    checkForConditions = VerifyIfMissingOperandIsCorrect(FirstOperand, CalculatedMissingOperand, SecondOperand, '/');
                 }
+                //when 22*2?=48? third operand is missing
                 else
                 {
-                    saveKnownInteger2 = Int32.Parse(splitOnEqualsSign[0]);
-                    saveUnknownInteger = splitOnEqualsSign[1];
-                    value = (saveKnownInteger2 * saveKnownInteger1);
-                    valueInString = value.ToString();
-                    if (saveKnownInteger2 * saveKnownInteger1 != value)
-                    {
-                        x = false;
-                    }
+                    SecondOperand = Int32.Parse(splitOnEqualsSign[0]);
+                    MissingOperand = splitOnEqualsSign[1];
+                    CalculatedMissingOperand = (FirstOperand * SecondOperand);
+                    CalculatedMissingOperandInString = CalculatedMissingOperand.ToString();
+                    checkForConditions = VerifyIfMissingOperandIsCorrect(FirstOperand, SecondOperand, CalculatedMissingOperand, '*');
                 }
             }
-            if (saveUnknownInteger.Length == valueInString.Length && x == true)
+            if (MissingOperand.Length == CalculatedMissingOperandInString.Length && checkForConditions == 1)
             {
-                int y = 0;
-                for (int i = 0; i < saveUnknownInteger.Length; i++)
-                {
-                    if (saveUnknownInteger[i] == '?')
-                    {
-                        y = Int32.Parse("" + valueInString[i]);
-                    }
-                }
-                return y;
+                return FetchingMissingIntegerValue(MissingOperand, CalculatedMissingOperandInString);
             }
             else
             {
                 return -1;
             }
-            throw new NotImplementedException();
+        }
+
+        //function to fetch the missing value from given missing operand and calculated operand
+
+        private static int FetchingMissingIntegerValue(String MissingOperand, String CalculatedMissingOperand)
+        {
+            // given equation 22*2?=484
+            //MissingOperand=2?
+            //CalculatedMissingOperand=22
+            int MissingIntegerValue = 0;
+            for (int i = 0; i < MissingOperand.Length; i++)
+            {
+                if (MissingOperand[i] == '?')
+                {
+                    MissingIntegerValue = Int32.Parse("" + CalculatedMissingOperand[i]);
+                }
+            }
+            return MissingIntegerValue;
+        }
+
+        //function to check if the calculated value of missing operand is correct or not
+
+        private static int VerifyIfMissingOperandIsCorrect(decimal FirstOperand, decimal SecondOperand,int ThirdOperand, char Operator)
+        {
+            try
+            {
+                //if equation is 22*2?=484
+                //then check 484/22 == 22
+                if (Operator == '/')
+                {
+                    if (Math.Ceiling(ThirdOperand / FirstOperand) != SecondOperand)
+                        return 0;
+                    else return 1;
+                }
+                //if equation is 22*22=48?
+                //then check 22*22 == 484
+                else if (Operator == '*')
+                {
+                    if (SecondOperand * FirstOperand != ThirdOperand)
+                        return 0;
+                    else return 1;
+                }
+                else return -1;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Cannot divide by zero");
+                return -1;
+            }
         }
     }
 }
